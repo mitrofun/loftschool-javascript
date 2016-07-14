4d580aacb0495c2d3f36cc1805115597fcfce9a5
mitri4 <mitri4@bk.ru>
2016-07-14 16:24:59 +0300

    jshint

--- homework-3/src/main.js
+++ homework-3/src/main.js
@@ -1,5 +1,5 @@
-arrFunc = require('./modules/functions');
-deepEqual = require('./modules/objects');
+let arrFunc = require('./modules/functions');
+let deepEqual = require('./modules/objects');
 
 let array = [1, 2, 3, 4, 5, 6];
 
--- homework-3/src/modules/objects.js
+++ homework-3/src/modules/objects.js
@@ -1,34 +1,35 @@
-function isNotObject(a, b) {
-    return a.valueOf() == b.valueOf() && typeof a != "object"
+function isSimple(a, b) {
+    return a.valueOf() == b.valueOf() && typeof a != "object";
 }
 
 function isIterable(a, b) {
-    return a.length == b.length && a.length && b.length
+    return a.length == b.length && a.length && b.length;
 }
 
 function isObject(a, b) {
-    return Object.getOwnPropertyNames(a).length == Object.getOwnPropertyNames(b).length
-        && typeof a == "object"
+    return Object.getOwnPropertyNames(a).length ==
+        Object.getOwnPropertyNames(b).length &&
+        typeof a == "object";
 }
 
 function deepEqual(sampleObj, compareObj) {
     let result = true;
-    if (isNotObject(sampleObj,compareObj)) {
+    if (isSimple(sampleObj, compareObj)) {
         result = true;
     } else if (isIterable(sampleObj, compareObj)) {
         for (let i = 0; i < sampleObj.length; i++) {
             result = deepEqual(sampleObj[i], compareObj[i]);
-            if (!result) { return false}
+            if (!result) { return false;}
         }
     } else if (isObject(sampleObj, compareObj)) {
         for (let key in sampleObj) {
         result = deepEqual(sampleObj[key], compareObj[key]);
-        if (!result) { return false}
+        if (!result) { return false;}
         }
     } else {
         return false;
     }
-    return result
+    return result;
 }
 
 module.exports = deepEqual;
