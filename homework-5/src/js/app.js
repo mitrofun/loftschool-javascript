649d817a87379d804844581bc4d42a01efa5dddb
mitri4 <mitri4@bk.ru>
2016-07-20 20:33:37 +0300

    babel

--- homework-5/src/js/app.js
+++ homework-5/src/js/app.js
@@ -1,49 +1,23 @@
-var buttonCreate = document.querySelector('.generator__btn');
-var buttonClean = document.querySelector('.generator__clean');
-var container = document.querySelector('.container__shape');
-let activeElement = null,
-    cX = 0,
-    cY = 0;
+import { toPx } from './modules/converter'
+import { getRandomNumber, getRandomColor } from './modules/random'
+import { mouseDown, mouseUp, mouseMove } from  './modules/drag-drop'
 
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+let buttonCreate = document.querySelector('.generator__btn');
+let buttonClean = document.querySelector('.generator__clean');
+let container = document.querySelector('.container__shape');
 
-function toPx(val) {
-    return val + 'px';
-}
-
-function getHex(val) {
-
-    var hex = val.toString(16);
-
-    if (hex.length == 1) {
-        hex = "0" + hex;
-    }
-
-    return hex;
-}
-
-function getRandomColor() {
-
-    var r = getRandom(0,255),
-        g = getRandom(0,255),
-        b = getRandom(0,255);
-
-    return "#" + getHex(r) + getHex(g) + getHex(b);
-}
 
 function createShape() {
-    var width, height, top ,left;
-    var shape = document.createElement('div');
-    var maxWidth = document.documentElement.clientWidth;
-    var maxHeight = document.documentElement.clientHeight;
+    let width, height, top ,left;
+    let shape = document.createElement('div');
+    let maxWidth = document.documentElement.clientWidth;
+    let maxHeight = document.documentElement.clientHeight;
+    let minVal = 21;
 
-
-    width = getRandom(0, maxWidth);
-    height = getRandom(0, maxHeight);
-    top = getRandom(0, maxHeight-height);
-    left = getRandom(0, maxWidth-width);
+    width = getRandomNumber(minVal, maxWidth);
+    height = getRandomNumber(minVal, maxHeight);
+    top = getRandomNumber(0, maxHeight-height);
+    left = getRandomNumber(0, maxWidth-width);
 
     shape.style.width = toPx(width);
     shape.style.height = toPx(height);
@@ -51,37 +25,15 @@ function createShape() {
     shape.style.left = toPx(left);
     shape.style.background = getRandomColor();
     shape.className = 'shape';
+
     container.appendChild(shape);
 }
 
 function removeAllShape() {
-    for (var i = container.childElementCount - 1; i >= 0; i--) {
-        console.log(container.children[i]);
+    for (let i = container.childElementCount - 1; i >= 0; i--) {
         container.removeChild(container.children[i]);
     }
 }
-
-function mouseDown(e) {
-    activeElement = e.target;
-    activeElement.ondragstart = function() {
-        return false;
-    };
-
-    cX = e.offsetX;
-    cY = e.offsetY;
-}
-
-function mouseUp() {
-    activeElement = null;
-}
-
-function mouseMove(e) {
-    if (activeElement) {
-        activeElement.style.left = toPx(e.clientX - cX);
-        activeElement.style.top = toPx(e.clientY - cY);
-    }
-}
-
 
 buttonCreate.addEventListener('click', createShape);
 buttonClean.addEventListener('click', removeAllShape);
---
+++ homework-5/src/js/modules/drag-drop.js
@@ -0,0 +1,27 @@
+let activeElement = null,
+    cX = 0,
+    cY = 0;
+
+function mouseDown(e) {
+    activeElement = e.target;
+    activeElement.ondragstart = function() {
+        return false;
+    };
+
+    cX = e.offsetX;
+    cY = e.offsetY;
+}
+
+function mouseUp() {
+    activeElement = null;
+}
+
+function mouseMove(e) {
+    if (activeElement) {
+        activeElement.style.left = toPx(e.clientX - cX);
+        activeElement.style.top = toPx(e.clientY - cY);
+    }
+}
+
+
+export { mouseDown, mouseUp, mouseMove }
