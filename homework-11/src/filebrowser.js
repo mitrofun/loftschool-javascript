ff71f0c034a6faaef52a9ea4a587ff29eed4e6cd
mitri4 <mitri4@bk.ru>
2016-08-29 23:47:03 +0300

    first part

--- homework-11/src/filebrowser.js
+++ homework-11/src/filebrowser.js
@@ -26,68 +26,6 @@ function scanPathToLog(path) {
     __scanPathToLog(path);
 }
 
-// scanPathToLog(PATH);
-
+scanPathToLog(PATH);
 
 module.export  = { scanPathToLog };
-
-let resumeObj = {'Roots': [] };
-
-let objDir = {
-    "name": "",
-    "type": "",
-    "fileType": "",
-    "size" :  "",
-    "nestedObjects": []
-};
-
-function __scanPathToJSON(path, obj) {
-    let dirs = fs.readdirSync(path);
-
-    obj = obj || objDir;
-
-    console.log(obj);
-
-    for (let dir of dirs) {
-        let pathToDir = `${path}/${dir}`;
-        let stat = fs.statSync(pathToDir);
-        let size = stat.size;
-
-        obj.name = dir;
-
-        if (stat.isDirectory()) {
-            obj.type = 'Folder';
-            // obj.nestedObjects.push(dir);
-            __scanPathToJSON(pathToDir, obj);
-        } else {
-            obj.type = 'File';
-            obj.fileType = dir.split('.')[dir.split('.').length-1]
-            obj.size = size;
-        }
-    }
-
-    return obj;
-}
-
-function scanPathToJSON(path) {
-    let json = __scanPathToJSON(path,'', objDir);
-    console.log('resume',json);
-}
-
-scanPathToJSON(PATH);
-
-// function getFiles (dir, files_){
-//     files_ = files_ || [];
-//     var files = fs.readdirSync(dir);
-//     for (var i in files){
-//         var name = dir + '/' + files[i];
-//         if (fs.statSync(name).isDirectory()){
-//             getFiles(name, files_);
-//         } else {
-//             files_.push(name);
-//         }
-//     }
-//     return files_;
-// }
-//
-// console.log(getFiles(PATH));
